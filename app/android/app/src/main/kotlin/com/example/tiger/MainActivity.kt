4037d5301c0e34ee827a00c818648baf7483fdb7
R. C. Howell <rchowell19@gmail.com>
2022-09-13 22:14:13 -0700

    Adds links for other windgrams

--- app/android/app/src/main/kotlin/com/example/tiger/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.github.rchowell.birdup
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- app/lib/pages/src/windgram_page.dart
+++ app/lib/pages/src/windgram_page.dart
@@ -1,44 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 
+// TODO draw all windgrams
 class WindgramPage extends StatelessWidget {
 
-  const WindgramPage({Key? key}) : super(key: key);
+  late final List<_Gram> grams;
+
+  WindgramPage({Key? key}) : super(key: key) {
+    // Force daily cache by adding a dummy key to the url
+    var now = DateTime.now();
+    var day = "${now.year}-${now.month}-${now.day}";
+    grams = [
+      _Gram(
+        label: 'I',
+        url: 'http://wxtofly.net/Tiger_windgram.png?day=$day',
+      ),
+      _Gram(
+        label: 'II',
+        url: 'http://wxtofly.net/TigerHikeLookout_windgram.png?day=$day',
+      ),
+      _Gram(
+        label: 'LZ',
+        url: 'http://wxtofly.net/TigerLZ_windgram.png?day=$day',
+      ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: Column(
-        children: const [
-          Text(
-            'Windgram',
-            style: TextStyle(
-              color: Colors.white,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.bold,
-              fontSize: 18.0,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 4.0),
-            child: Text(
-              "wxtofly.net",
-              style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontSize: 10.0,
+        appBar: AppBar(
+          title: Column(
+            children: const [
+              Text(
+                'Windgram',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
               ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 4.0),
+                child: Text(
+                  "wxtofly.net",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 10.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.black,
+        body: Center(
+          child: PhotoView(
+            imageProvider: const NetworkImage(
+              'http://wxtofly.net/Tiger_windgram.png',
             ),
           ),
-        ],
-      ),
-      backgroundColor: Colors.black,
-    ),
-    backgroundColor: Colors.black,
-    body: Center(
-      child: PhotoView(
-        imageProvider: const NetworkImage('http://wxtofly.net/Tiger_windgram.png',),
-      ),
-    ),
-  );
+        ),
+      );
+}
+
+class _Gram {
+  final String label;
+  final String url;
+
+  _Gram({
+    required this.label,
+    required this.url,
+  });
 }
